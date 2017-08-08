92691ad1b0211330402f22383d9f0eb9398b2516
Ospens <1ospens1@gmail.com>
2017-08-08 18:36:11 +0300

    Глава 10 пройдена

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,16 @@
 class StoreController < ApplicationController
+
+  def session_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+
+  end
   def index
+
     @products = Product.order(:title)
+    @count = session_counter
+    @count_message = 'views ' + @count.to_s
   end
 end
